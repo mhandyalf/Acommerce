6a7c4ec73621b544aec3ed44d57329ee04de74d6
mhandyalf <mhandyalfurqon@gmail.com>
2023-09-05 15:03:04 +0700

    adding jwt middleware

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -3,7 +3,9 @@ package handlers
 import (
 	"acommerce/models"
 	"net/http"
+	"time"
 
+	"github.com/golang-jwt/jwt/v4"
 	"github.com/labstack/echo/v4"
 	"golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm"
@@ -41,4 +43,68 @@ func (a *Auth) Register(e echo.Context) error {
 
 	return e.JSON(http.StatusCreated, user)
 
+}
+
+func (a *Auth) Login(e echo.Context) error {
+	user := new(models.User)
+	if err := e.Bind(user); err != nil {
+		return e.JSON(http.StatusBadRequest, map[string]string{
+			"message": err.Error(),
+		})
+	}
+
+	var foundUser models.User
+	if err := a.db.Where("username = ?", user.Username).First(&foundUser).Error; err != nil {
+		return e.JSON(http.StatusUnauthorized, map[string]string{
+			"message": err.Error(),
+		})
+	}
+
+	if err := bcrypt.CompareHashAndPassword([]byte(foundUser.Password), []byte(user.Password)); err != nil {
+		return e.JSON(http.StatusUnauthorized, map[string]string{
+			"message": err.Error(),
+		})
+	}
+
+	// Buat token JWT
+	token := jwt.New(jwt.SigningMethodHS256)
+	claims := token.Claims.(jwt.MapClaims)
+	claims["username"] = foundUser.Username
+	claims["exp"] = time.Now().Add(time.Hour * 24).Unix() // Token berlaku selama 1 hari
+
+	// Sign token dengan secret key
+	tokenString, err := token.SignedString([]byte("secret-key")) // Gantilah "secret-key" dengan kunci rahasia yang lebih kuat
+
+	if err != nil {
+		return e.JSON(http.StatusInternalServerError, map[string]string{
+			"message": "Gagal membuat token JWT",
+		})
+	}
+
+	// Kirim token sebagai respons
+	return e.JSON(http.StatusOK, map[string]string{
+		"token": tokenString,
+	})
+}
+
+func (a *Auth) GetProducts(e echo.Context) error {
+	var products []models.Product
+	if err := a.db.Find(&products).Error; err != nil {
+		return e.JSON(http.StatusInternalServerError, map[string]string{
+			"message": err.Error(),
+		})
+	}
+
+	return e.JSON(http.StatusOK, products)
+}
+
+func (a *Auth) GetTransactions(e echo.Context) error {
+	var transactions []models.Transaction
+	if err := a.db.Find(&transactions).Error; err != nil {
+		return e.JSON(http.StatusInternalServerError, map[string]string{
+			"message": err.Error(),
+		})
+	}
+
+	return e.JSON(http.StatusOK, transactions)
 }
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"acommerce/database"
 	"acommerce/handlers"
+	"acommerce/middleware"
 
 	"github.com/labstack/echo/v4"
 )
@@ -12,6 +13,9 @@ func main() {
 
 	authhandler := handlers.NewAuth(database.InitDB())
 	e.POST("/register", authhandler.Register)
+	e.POST("/login", authhandler.Login)
+	e.GET("/products", authhandler.GetProducts, middleware.JWTAuth)
+	e.GET("/transactions", authhandler.GetTransactions, middleware.JWTAuth)
 
 	e.Logger.Fatal(e.Start(":8080"))
 
